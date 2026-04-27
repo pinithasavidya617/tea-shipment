234a94193b9a3fc24e750ddd123ae36a78bf2987
Pinitha Savidya <savidyabalasooriya617@gmail.com>
2026-04-27 14:48:22 +0530

    Add constructor chain

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        TeaShipment teaShipment1 = new TeaShipment("BOPF", 20, "UK", "Watawala");
-        System.out.println(teaShipment1.grade);
+//        TeaShipment teaShipment1 = new TeaShipment("BOPF", 20, "UK", "Watawala");
+//        TeaShipment teaShipment2 = new TeaShipment(20, "UK", "Watawala");
+//        TeaShipment teaShipment3 = new TeaShipment("UK", "Watawala");
+
+        TeaShipment teaShipment4 = new TeaShipment();
+
+        System.out.println(teaShipment4.grade);
 
     }
 }
--- src/TeaShipment.java
+++ src/TeaShipment.java
@@ -1,13 +1,50 @@
 public class TeaShipment {
-     String grade;
-     int weight;
-     String shipment;
-     String exporterName;
+     private String grade;
+     private int weight;
+     private String shipmentCountry;
+     private String exporterName;
 
-     public TeaShipment(String grade, int weight, String shipment, String exporterName){
-         this.grade = grade;
-         this.weight = weight;
-         this.shipment = shipment;
-         this.exporterName = exporterName;
-     }
+    public  TeaShipment(){
+        this("Ceylon Tea Export");
+    }
+
+    public TeaShipment(String exporterName){
+        this("UK", exporterName);
+    }
+
+    public TeaShipment(String shipmentCountry, String exporterName){
+        this(20, shipmentCountry, exporterName);
+    }
+
+    public TeaShipment(int weight, String shipmentCountry, String exporterName){
+        this("BOPF", weight, shipmentCountry, exporterName);
+
+//        System.out.println("executing constructor taking 3 arguments");
+    }
+
+    public TeaShipment(String grade, int weight, String shipmentCountry, String exporterName){
+        this.grade = grade;
+        this.weight = weight;
+        this.shipmentCountry = shipmentCountry;
+        this.exporterName = exporterName;
+
+//        System.out.println("executing constructor taking 4 arguments");
+    }
+
+    public void printGrade(){
+        System.out.println(this.grade);
+    }
+
+    public void printWeight(){
+        System.out.println(this.weight);
+    }
+
+    public void printCountry(){
+        System.out.println(this.shipmentCountry);
+
+    }public void printExporter(){
+        System.out.println(this.exporterName);
+    }
+
+
 }
